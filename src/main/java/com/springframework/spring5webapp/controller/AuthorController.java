8864b256a47278495c526139fb028b83c36b85ee
serkan <serkanguler.cs@gmail.com>
2021-05-11 17:38:06 +0300

    Author list page has been added by using thymeleaf.

---
+++ src/main/java/com/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.springframework.spring5webapp.controller;
+
+import com.springframework.spring5webapp.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors/list";
+    }
+}
