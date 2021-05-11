1c9db52179dc34e1ecb79418e27534a3aecac68a
serkan <serkanguler.cs@gmail.com>
2021-05-11 17:02:04 +0300

    BookController has been added.

---
+++ src/main/java/com/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.springframework.spring5webapp.controller;
+
+import com.springframework.spring5webapp.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
